98e1c6ac975b1b4b188ba005f6c01731ed02ecb4
mhusse29 <mohamedhusseins598@gmail.com>
2025-09-14 17:15:16 -0400

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/media-planner-lite/', // Replace with your repository name
   test: {
     globals: true,
     environment: 'jsdom',
